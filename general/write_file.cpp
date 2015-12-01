97514d7c845b4468839739c4423f6cc2449855e6
PureAwesomeness <johannesbatzill@live.de>
2015-12-01 21:51:02 +0100

    added write file and fixed ccw

---
+++ general/write_file.cpp
@@ -0,0 +1,4 @@
+ofstream myfile;
+myfile.open("42.in");
+myfile << "blabliblablub"
+myfile.close();
--- geometry/basics/ccw.cpp
+++ geometry/basics/ccw.cpp
@@ -1,7 +1,7 @@
 //p1 -> p2 -> p3
 //ccw(=counter clockwise) > 0
 //colinear == 0
-//cw > 0
+//cw < 0
 ll ccw(point2 p1, point2 p2, point2 p3) {
     return X2(p2-p1, p3-p1);
 }
