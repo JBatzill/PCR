77d63d6537d570c09a789ce1bef3d8acafa26117
PureAwesomeness <johannesbatzill@live.de>
2016-02-20 19:02:57 +0100

    Added scanf and printf exp, splitted usefull file, moved miller rabin

---
+++ general/read_write/console/scanf_printf.cpp
@@ -0,0 +1,4 @@
+//reads from std input
+scanf(str,&v1,&v2);
+//writes on std output
+printf(str,v1,v2);
---
+++ general/read_write/optimize.cpp
@@ -0,0 +1,7 @@
+//USE FOR FAST IO
+ios::sync_with_stdio(false);
+//UNTIES CIN AND COUT (IF INTERACTIVE: USE FLUS EXPLICITLY)
+cin.tie(NULL);
+cout.flush();
+//USE FOR PRECISE OUTPUT
+setprecision(10); fixed;
